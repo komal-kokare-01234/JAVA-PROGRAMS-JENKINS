6cbb0ff56b733229dbc3918b7c6453ba7eba59b7
Komal Mahadeo Kokare <komal.kokare01234@gmail.com>
2026-04-10 00:44:29 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("This is Java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
+        System.out.println("hello");
     }
 }
